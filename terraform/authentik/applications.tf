89180d72620549d630323c62ab9ffa89b26a594c
Jacob Williams <jacob.t.williams@outlook.com>
2025-01-06 08:49:56 -0600

    Deploy immich config

--- terraform/authentik/applications.tf
+++ terraform/authentik/applications.tf
@@ -1,6 +1,6 @@
 
 locals {
-  icon_base                   = "https://raw.githubusercontent.com/ramblurr/home-ops/main/icons"
+  icon_base                   = "https://raw.githubusercontent.com/novamachina-mods/home-ops/main/icons"
   implicit_authorization_flow = resource.authentik_flow.provider-authorization-implicit-consent.uuid
   explicit_authorization_flow = data.authentik_flow.default-provider-authorization-explicit-consent.id
   default_authentication_flow = data.authentik_flow.default-authentication-flow.id
@@ -35,6 +35,23 @@ module "grafana" {
   authentication_flow_id = local.default_authentication_flow
   invalidation_flow_id   = local.default_invalidation_flow
   redirect_uris          = ["https://grafana.${var.internal_domain}/login/generic_oauth"]
+  property_mappings      = data.authentik_property_mapping_provider_scope.oauth2.ids
+  access_token_validity  = "hours=4"
+  authentik_domain       = var.authentik_domain
+  vault                  = local.onepassword_vault_id
+  meta_icon              = "${local.icon_base}/grafana.png"
+}
+
+module "immich" {
+  source                 = "./modules/oidc-application"
+  name                   = "immich-test"
+  client_id              = "immich-test"
+  domain                 = "immich-test.${var.internal_domain}"
+  group                  = "Books"
+  authorization_flow_id  = local.implicit_authorization_flow
+  authentication_flow_id = local.default_authentication_flow
+  invalidation_flow_id   = local.default_invalidation_flow
+  redirect_uris          = ["https://grafana.${var.internal_domain}/auth/login"]
   property_mappings      = data.authentik_property_mapping_provider_scope.oauth2.ids
   access_token_validity  = "hours=4"
   authentik_domain       = var.authentik_domain
