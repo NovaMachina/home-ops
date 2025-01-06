0e865ca8cc2feb6ea0dbea7010c5d1c36536d10f
Jacob Williams <jacob.t.williams@outlook.com>
2025-01-06 08:57:41 -0600

    Fix immich config

--- terraform/authentik/applications.tf
+++ terraform/authentik/applications.tf
@@ -1,6 +1,6 @@
 
 locals {
-  icon_base                   = "https://raw.githubusercontent.com/novamachina-mods/home-ops/main/icons"
+  icon_base                   = "https://raw.githubusercontent.com/NovaMachina/home-ops/main/icons"
   implicit_authorization_flow = resource.authentik_flow.provider-authorization-implicit-consent.uuid
   explicit_authorization_flow = data.authentik_flow.default-provider-authorization-explicit-consent.id
   default_authentication_flow = data.authentik_flow.default-authentication-flow.id
@@ -51,10 +51,10 @@ module "immich" {
   authorization_flow_id  = local.implicit_authorization_flow
   authentication_flow_id = local.default_authentication_flow
   invalidation_flow_id   = local.default_invalidation_flow
-  redirect_uris          = ["https://grafana.${var.internal_domain}/auth/login"]
+  redirect_uris          = ["https://immich-test.${var.internal_domain}/auth/login"]
   property_mappings      = data.authentik_property_mapping_provider_scope.oauth2.ids
   access_token_validity  = "hours=4"
   authentik_domain       = var.authentik_domain
   vault                  = local.onepassword_vault_id
-  meta_icon              = "${local.icon_base}/grafana.png"
+  meta_icon              = "${local.icon_base}/immich.png"
 }
