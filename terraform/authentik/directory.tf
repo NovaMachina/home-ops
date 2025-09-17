eec0e3fced37e4e00bfbf54cf8952e1126f965ee
Jacob Williams <jacob.t.williams@outlook.com>
2025-09-16 21:38:46 -0500

    Remove flux endpoint

--- terraform/authentik/directory.tf
+++ terraform/authentik/directory.tf
@@ -1,7 +1,7 @@
 locals {
   authentik_groups = {
     downloads      = { name = "Downloads" }
-    grafana_admin  = { name = "Grafana Admin" }
+    grafana_admin  = { name = "Grafana Admins" }
     home           = { name = "Home" }
     infrastructure = { name = "Infrastructure" }
     media          = { name = "Media" }
